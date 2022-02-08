aec23f0497b8a0f18678f7174450d576fa7e6e3f
William96123 <duanbwilliam@gmail.com>
2022-02-07 20:58:38 -0800

    Began working on problem set 3 question 1

--- src/E1.jl
+++ src/E1.jl
@@ -1,5 +1,35 @@
 module E1
+export rowmatmul!, colmatmul!, rowmatmuldot!, colmatmuldot!
 
-# Write your package code here.
+import LinearAlgebra:dot
+
+function rowmatmul!(C, A, B)
+    for i in 1:size(C, 1)
+        for j in 1:size(C,2)
+            C[i,j] = zero(eltype(C))
+            for k in 1:size(A,2)
+                C[i, j] += A[i,k]*B[k,j]
+            end
+        end
+    end
+end
+
+function colmatmul!(C, A, B)
+    for j in 1:size(C, 1)
+        for i in 1:size(C, 2)
+            C[i,j] = zero(eltype(C))
+            for k in 1:size(A,2)
+                C[i, j] += A[i,k]*B[k,j]
+            end
+        end
+    end
+end
+
+function rowmatmuldot!(C,A,B)
+
+end
+
+function colmatmuldot!(C,A,B)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,27 @@ using Test
 
 @testset "E1.jl" begin
     # Write your tests here.
+    n = 10
+    A = randn(n,n)
+    B = randn(n,n)
+    C = A*B
+    Ctest = similar(C)
+    rowmatmul!(Ctest, A, B)
+    @test Ctest ≈ C
+    colmatmul!(Ctest, A, B)
+    @test Ctest ≈ C
+
+end
+
+@testset "dot versions" begin
+        # Write your tests here.
+        n = 10
+        A = randn(n,n)
+        B = randn(n,n)
+        C = A*B
+        Ctest = similar(C)
+        rowmatmuldot!(Ctest, A, B)
+        @test Ctest ≈ C
+        colmatmuldot!(Ctest, A, B)
+        @test Ctest ≈ C    
 end
